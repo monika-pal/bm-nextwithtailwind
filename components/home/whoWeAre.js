1357a9b896156ee5d9e3fc5e5d85b222e8a8cd26
monika-pal <monika.pal@moglix.com>
2022-12-15 15:46:43 +0530

    some parallax effect and animation

--- components/home/whoWeAre.js
+++ components/home/whoWeAre.js
@@ -9,9 +9,9 @@ export default function whoWeAre() {
             <h2 className='text-headingSmall font-medium mb-[40px] text-brand'>
               Who we are
             </h2>
-            <p className='lg:text-heading1 text-heading2 text-secondary font-semibold'>Moglix is an end-to end supply chain solution company focused on tech-enabled MRO sourcing, custom manufacturing procurement and infrastructure industry supplies.</p>
+            <p className='lg:text-heading2 text-heading1 text-secondary font-semibold'>Moglix is an end-to end supply chain solution company focused on tech-enabled MRO sourcing, custom manufacturing procurement and infrastructure industry supplies.</p>
           </div>
-          <div className="w-3/5  pl-[200px] pr-[100px]">
+          <div className="w-3/5  pl-[200px] ">
            <p className='mt-[60px] mb-[15px] lg:text-xxl text-xl'>From efficient manufacturing plants, resilient supply chains, and smarter buildings, to smoother and efficient procurement, we enable enterprises to focus on what they do best.</p>
            <p className="lg:text-xxl text-xl">By coalescing the real and the virtual worlds, we empower businesses to transform their operations and distribution helping them touch billions of lives everyday.</p>
           </div>
